f3f80bb12f696c35b9572264208b8be4e49745fa
max-student <max-student@maxtrain.com>
2021-02-15 14:34:08 -0500

    Add project files.

---
+++ Widget/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Widget {
+    class Program {
+        static void Main(string[] args) {
+
+           var w1 = new Widget("LARGE", "RED");
+            Console.WriteLine($"A {w1.Size}, {w1.Color} widget costs {w1.Price()}");
+
+            var w2 = new Widget("SMALL", "WHITE");
+            Console.WriteLine($"A {w2.Size}, {w2.Color} widget costs {w2.Price()}");
+
+
+        }
+    }
+}
---
+++ Widget/Widget.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Widget {
+    class Widget {
+        // properties
+
+        public String Size { get; set; }
+        public String Color { get; set; }
+
+        // methods = calculate price
+
+        public int Price() {
+            var price = 0;
+            if (Size == "large") {
+                price += 20;
+            }
+            if (Size == "medium") {
+                price = +15;
+            }
+            if (Size == "small") {
+                price += 5;
+            }
+
+            if (Color == "red") {
+                price += 15;
+            }
+
+            if (Color == "white") {
+                price += 10;
+            }
+            if (Color == "blue") {
+                price += 5;
+            }
+
+            return price;
+        }
+
+
+        public Widget() { 
+        }
+
+        public Widget(string size, string color) {
+            Size = size.ToLower();
+            Color = color.ToLower();
+        } 
+    }
+}
