7fbce4b13e2f4c9bfdef18cd0952d266143ae886
max-student <max-student@maxtrain.com>
2021-02-20 16:08:22 -0500

    commit

--- Widget/Program.cs
+++ Widget/Program.cs
@@ -11,7 +11,15 @@ namespace Widget {
             Console.WriteLine($"A {w2.Size}, {w2.Color} widget costs {w2.Price()}");
 
             var w3 = new Widget("MEDIUM", "BLUE");
-            Console.WriteLine($"A {w3.Size}, {w3.Color} widget costs{w3.Price()}");
+            Console.WriteLine($"A {w3.Size}, {w3.Color} widget costs {w3.Price()}");
+
+            var w4 = new Widget("LARGE", "WHITE");
+
+            Console.WriteLine($"A {w4.Size}, {w4.Color}, widget costs {w4.Price()}");
+
+            var w5 = new Widget("SMALL", "RED");
+
+            Console.WriteLine($"A {w5.Size}, {w5.Color}, widget costs {w5.Price()}");
         }
     }
 }
